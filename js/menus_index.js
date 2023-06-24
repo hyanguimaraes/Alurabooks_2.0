36cc9dbe0aa8f5396f5c6091f8cfc11dc0b5259c
hyanguimaraes <hyangt@hotmail.com>
2023-06-24 09:02:24 -0300

    Criação do botão Usuário e adaptação do CSS e JS.

--- js/menus_index.js
+++ js/menus_index.js
@@ -1,13 +1,23 @@
 //JAVASCRIPT PARA MENUS
 
-//MENU LATERAL
-const botaoMenu = document.querySelector('.cabecalho_menu_hamburguer');
-const menu = document.querySelector('.lista_menu');
+//MENU HAMBÚRGUER
+const botaoMenuHamburguer = document.querySelector('.cabecalho_menu_hamburguer');
+const menuHamburguer = document.querySelector('.lista_menu_hamburger');
 const paginacao = document.querySelector('.swiper-pagination')
 
-botaoMenu.addEventListener('click', ()=> {
-    menu.classList.toggle('lista_menu_ativo');
-    botaoMenu.classList.toggle('cabecalho_menu_hamburguer_ativo')
+botaoMenuHamburguer.addEventListener('click', ()=> {
+    menuHamburguer.classList.toggle('lista_menu_hamburguer_ativo');
+    botaoMenuHamburguer.classList.toggle('cabecalho_menu_hamburguer_ativo')
+    paginacao.classList.toggle('swiper-pagination_menu_ativo')
+});
+
+//MENU USUÁRIO
+const botaoMenuUsuario = document.querySelector('.cabecalho_menu_usuario');
+const menuUsuario = document.querySelector('.lista_menu_usuario');
+
+botaoMenuUsuario.addEventListener('click', ()=> {
+    menuUsuario.classList.toggle('lista_menu_usuario_ativo');
+    botaoMenuUsuario.classList.toggle('cabecalho_menu_usuario_ativo')
     paginacao.classList.toggle('swiper-pagination_menu_ativo')
 });
 
@@ -15,6 +25,6 @@ botaoMenu.addEventListener('click', ()=> {
 const botaoCategorias = document.querySelector('.cabecalho_opcoes_botao')
 
 botaoCategorias.addEventListener('click', ()=>{
-    menu.classList.toggle('lista_menu_ativo');
+    menuHamburguer.classList.toggle('lista_menu_ativo');
     botaoCategorias.classList.toggle('cabecalho_opcoes_botao_ativo');
 })
