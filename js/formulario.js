c541d0cb61e5a7a7898000cbb5ed464b42e26b37
hyanguimaraes <hyangt@hotmail.com>
2023-06-27 19:39:06 -0300

    Atualização dos arquivos ao final da Aula 04.

--- js/formulario.js
+++ js/formulario.js
@@ -68,7 +68,9 @@ else {
 }
 }; */
 
-var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
+
+// MÉTODO 01 //
+/* var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
     .then(resposta => resposta.json())
     .then(r => {
         if (r.erro) {
@@ -80,4 +82,20 @@ var consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/')
     .catch(erro => console.log(erro))
     .finally(mensagem => console.log('Processamento concluído'));
 
-console.log(consultaCEP);
+console.log(consultaCEP); */
+
+
+// MÉTODO 02 //
+async function buscaEndereco(cep){
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro){
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
